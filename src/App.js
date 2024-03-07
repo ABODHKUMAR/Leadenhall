345632bb1872c2f59608fe3dc0cce70b175a69a9
abodh5921 <abodh5921@gmail.com>
2024-03-07 19:05:50 +0530

    Applied Css Code Working Fine

--- src/App.js
+++ src/App.js
@@ -5,31 +5,67 @@ import { BrokerStatusData } from "./BrokerStatusData";
 import BarChart from "./components/BarChart.js";
 import BusinessClassAnalysis from "./businessclass/BusinessClassAnalysis.js";
 import { BusinessClassDate } from "./businessclass/BusinessClassData.js";
+
 function App() {
-  // Filter data based on the "Open Market" parameter
   const openMarketData = BrokerStatusData.filter(
     (data) => data["Market Type"] === "Open Market"
   );
-  const FacilitiesData = BrokerStatusData.filter(
+  const facilitiesData = BrokerStatusData.filter(
     (data) => data["Market Type"] === "Facilities"
   );
 
-  // Sort the filtered data based on GWP
   const sortedBrokers = openMarketData.sort((a, b) => b.GWP - a.GWP);
-  const sortedBrokersFacilities = FacilitiesData.sort((a, b) => b.GWP - a.GWP);
-  const sortedBrokersFacilitiesAndOpenMarket = BrokerStatusData.sort((a, b) => b.GWP - a.GWP); 
+  const sortedFacilitiesBrokers = facilitiesData.sort((a, b) => b.GWP - a.GWP);
+  const sortedCombinedBrokers = BrokerStatusData.sort((a, b) => b.GWP - a.GWP);
 
-  // Select the top 10 brokers
   const top10Brokers = sortedBrokers.slice(0, 10);
-  const top10FacilitiesBrokers = sortedBrokersFacilities.slice(0, 10);
-  const top10CombinedBrokers = sortedBrokersFacilitiesAndOpenMarket.slice(0, 10);
+  const top10FacilitiesBrokers = sortedFacilitiesBrokers.slice(0, 10);
+  const top10CombinedBrokers = sortedCombinedBrokers.slice(0, 10);
 
-  const [userData, setUserData] = useState({
+  const [BrokersData, setBrokersData] = useState({
     labels: top10Brokers.map((data) => data.Year),
     datasets: [
       {
-        label: "Users Gained",
+        label: "GWP",
         data: top10Brokers.map((data) => data.GWP),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  const [top10FacilitiesData, setTop10FacilitiesData] = useState({
+    labels: top10FacilitiesBrokers.map((data) => data.Year),
+    datasets: [
+      {
+        label: "GWP",
+        data: top10FacilitiesBrokers.map((data) => data.GWP),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  const [top10CombinedData, setTop10CombinedData] = useState({
+    labels: top10CombinedBrokers.map((data) => data.Year),
+    datasets: [
+      {
+        label: "GWP",
+        data: top10CombinedBrokers.map((data) => data.GWP),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#ecf0f1",
@@ -45,83 +81,97 @@ function App() {
 
   return (
     <div className="App">
-      <div className="horizontal-tables-container">
-        <div>
-          <h2>Open Market Data</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Year</th>
-                <th>GWP</th>
-                <th>Planned GWP</th>
-                <th>Market Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {top10Brokers.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.Year}</td>
-                  <td>{data.GWP}</td>
-                  <td>{data["Planned GWP"]}</td>
-                  <td>{data["Market Type"]}</td>
-                </tr>
-              ))}
-              <div style={{ width: 700 }}>
-              <BarChart chartData={userData} />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-4 col-md-6">
+            <div className="card mt-4">
+              <div className="card-body">
+                <h5 className="card-title text-center">OPEN MARKET</h5>
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Year</th>
+                      <th>BROKER</th>
+                      <th>GWP</th>
+                      <th>[GWP-PGWP]%</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {top10Brokers.map((data, index) => (
+                      <tr key={index}>
+                        <td>{data.Year}</td>
+                        <td>{data["Broker Name"]}</td>
+                        <td>{data.GWP}</td>
+                        <td>{(((data.GWP - data["Planned GWP"]) / data["Planned GWP"]) * 100).toFixed(1)}%</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <BarChart chartData={BrokersData} />
               </div>
-            </tbody>
-          </table>
-        </div>
+            </div>
+          </div>
 
-        <div>
-          <h2>Facilities Data</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Year</th>
-                <th>GWP</th>
-                <th>Planned GWP</th>
-                <th>Market Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {top10FacilitiesBrokers.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.Year}</td>
-                  <td>{data.GWP}</td>
-                  <td>{data["Planned GWP"]}</td>
-                  <td>{data["Market Type"]}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="card mt-4">
+              <div className="card-body">
+                <h5 className="card-title text-center">FACILITIES</h5>
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Year</th>
+                      <th>BROKER</th>
+                      <th>GWP</th>
+                      <th>[GWP-PGWP]%</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {top10FacilitiesBrokers.map((data, index) => (
+                      <tr key={index}>
+                        <td>{data.Year}</td>
+                        <td>{data["Broker Name"]}</td>
+                        <td>{data.GWP}</td>
+                        <td>{(((data.GWP - data["Planned GWP"]) / data["Planned GWP"]) * 100).toFixed(1)}%</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <BarChart chartData={top10FacilitiesData} />
+              </div>
+            </div>
+          </div>
 
-        <div>
-          <h2>Combined Data (Open Market and Facilities)</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Year</th>
-                <th>GWP</th>
-                <th>Planned GWP</th>
-                <th>Market Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {top10CombinedBrokers.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.Year}</td>
-                  <td>{data.GWP}</td>
-                  <td>{data["Planned GWP"]}</td>
-                  <td>{data["Market Type"]}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="col-lg-4 col-md-6">
+            <div className="card mt-4">
+              <div className="card-body">
+                <h5 className="card-title text-center">OPEN MARKET AND FACILITIES</h5>
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Year</th>
+                      <th>BROKER</th>
+                      <th>GWP</th>
+                      <th>[GWP-PGWP]%</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {top10CombinedBrokers.map((data, index) => (
+                      <tr key={index}>
+                        <td>{data.Year}</td>
+                        <td>{data["Broker Name"]}</td>
+                        <td>{data.GWP}</td>
+                        <td>{(((data.GWP - data["Planned GWP"]) / data["Planned GWP"]) * 100).toFixed(1)}%</td>
+
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <BarChart chartData={top10CombinedData} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
       <BusinessClassAnalysis data={BusinessClassDate} />
     </div>
   );
--- src/businessclass/BusinessClassAnalysis.js
+++ src/businessclass/BusinessClassAnalysis.js
@@ -1,13 +1,13 @@
-// BusinessClassAnalysis.js
 import React, { useState } from "react";
-import "./BusinessClassAnalysis.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const BusinessClassAnalysis = ({ data }) => {
   const [selectedClass, setSelectedClass] = useState(null);
   const [selectedClassType, setSelectedClassType] = useState(null);
 
   const handleClassClick = (classOfBusiness) => {
     setSelectedClass(classOfBusiness);
-    setSelectedClassType(null); // Reset selected class type when a new class of business is selected
+    setSelectedClassType(null);
   };
 
   const handleClassTypeClick = (classType) => {
@@ -40,62 +40,69 @@ const BusinessClassAnalysis = ({ data }) => {
       : data;
 
   return (
-    <div>
-      <h2>Business Class Analysis</h2>
-      <div>
-        <h3>Classes of Business</h3>
-        <ul>
-          {classOfBusinessOptions.map((classOfBusiness, index) => (
-            <li key={index} onClick={() => handleClassClick(classOfBusiness)}>
-              {classOfBusiness}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {selectedClass && (
-        <div>
-          <h3>Class Types</h3>
-          <ul>
-            {classTypeOptions.map((classType, index) => (
+    <div className="container-fluid">
+      <h2 className="mt-4 mb-4">BUSINESS CLASS ANALYSIS</h2>
+      <div className="row">
+        <div className="col-lg-3">
+          <h3>CLASSES OF BUSINESS</h3>
+          <ul className="list-group">
+            {classOfBusinessOptions.map((classOfBusiness, index) => (
               <li
                 key={index}
-                onClick={() => handleClassTypeClick(classType)}
-                className={classType === selectedClassType ? "selected" : ""}
+                className="list-group-item"
+                onClick={() => handleClassClick(classOfBusiness)}
               >
-                {classType}
+                {classOfBusiness}
               </li>
             ))}
           </ul>
         </div>
-      )}
-
-      <div>
-        <h3>Analysis Results</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Year</th>
-              <th>Class of Business</th>
-              <th>Class Type</th>
-              <th>Business Plan</th>
-              <th>Earned Premium</th>
-              <th>GWP</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData.map((item, index) => (
-              <tr key={index}>
-                <td>{item.Year}</td>
-                <td>{item["Class of Business"]}</td>
-                <td>{item.ClassType}</td>
-                <td>{item["Business Plan"]}</td>
-                <td>{item["Earned Premium"]}</td>
-                <td>{item.GWP}</td>
+        {selectedClass && (
+          <div className="col-lg-3">
+            <h3>CLASS TYPES</h3>
+            <ul className="list-group">
+              {classTypeOptions.map((classType, index) => (
+                <li
+                  key={index}
+                  className={
+                    "list-group-item" +
+                    (classType === selectedClassType ? " active" : "")
+                  }
+                  onClick={() => handleClassTypeClick(classType)}
+                >
+                  {classType}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <div className="col-lg-6">
+          <h3>ANALYSIS RESULTS</h3>
+          <table className="table table-striped">
+            <thead className="thead-dark">
+              <tr>
+                <th>Year</th>
+                <th>Class of Business</th>
+                <th>Class Type</th>
+                <th>Business Plan</th>
+                <th>Earned Premium</th>
+                <th>GWP</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.Year}</td>
+                  <td>{item["Class of Business"]}</td>
+                  <td>{item.ClassType}</td>
+                  <td>{item["Business Plan"]}</td>
+                  <td>{item["Earned Premium"]}</td>
+                  <td>{item.GWP}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
